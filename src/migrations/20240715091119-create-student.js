c83ae555cdc56df2c1187b200e6127d1e1b585e8
mrskrsm <francescoforgiarini03@gmail.com>
2024-07-15 11:16:12 +0200

    add: model student

---
+++ src/migrations/20240715091119-create-student.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      surename: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      date_birth: {
+        type: Sequelize.DATEONLY
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phone_number: {
+        type: Sequelize.STRING
+      },
+      notes: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('students');
+  }
+};
---
+++ src/models/student.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class student extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  student.init({
+    surename: DataTypes.STRING,
+    name: DataTypes.STRING,
+    date_birth: DataTypes.DATEONLY,
+    email: DataTypes.STRING,
+    phone_number: DataTypes.STRING,
+    notes: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'student',
+  });
+  return student;
+};

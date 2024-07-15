bd0db38c068e05be70d2f89a774cadf7af234841
mrskrsm <francescoforgiarini03@gmail.com>
2024-07-15 12:00:27 +0200

    yes

--- src/migrations/studentsList.js
+++ src/migrations/studentsList.js
@@ -1,122 +1,98 @@
 module.exports = [
   {
-    "surname": "Rossi",
-    "name": "Marco",
-    "date_birth": "2003-07-15",
-    "email": "marco.rossi@example.com",
-    "phone_number": "+39 328 123 4567",
-    "notes": "Studente di ingegneria informatica."
+    surename: "Foà",
+    name: "Sabatino",
+    date_birth: "2003-11-14",
+    email: "sabatino.foà@libero.it",
+    phone_number: "+39 41 10484185",
+    notes: "Nulla qui ipsum nihil rem qui. Fuga consequuntur eius molestias quis. Iste sed inventore sit possimus dolores."
   },
   {
-    "surname": "Bianchi",
-    "name": "Luca",
-    "date_birth": "2002-04-10",
-    "email": "luca.bianchi@example.com",
-    "phone_number": "+39 327 987 6543",
-    "notes": "Studente di economia aziendale."
+    surename: "Boccioni",
+    name: "Silvio",
+    date_birth: "2003-08-08",
+    email: "silvio.boccioni@tim.it",
+    phone_number: "+39 9187 18590644",
+    notes: "Distinctio totam laborum omnis. Quas ab nobis repellendus. Molestias debitis blanditiis ipsam. Officia maiores velit. Amet temporibus architecto occaecati a."
   },
   {
-    "surname": "Ferrari",
-    "name": "Giulia",
-    "date_birth": "2003-11-20",
-    "email": "giulia.ferrari@example.com",
-    "phone_number": "+39 320 123 0987",
-    "notes": "Studentessa di medicina."
+    surename: "Martucci",
+    name: "Ernesto",
+    date_birth: "2006-04-05",
+    email: "ernesto.martucci@poste.it",
+    phone_number: "+39 71 91745677",
+    notes: "Veniam voluptatum ullam doloribus recusandae. Omnis placeat quasi fuga assumenda maiores minus. Rerum quam magnam odio dolores fugiat. Omnis mollitia aspernatur unde eveniet ducimus."
   },
   {
-    "surname": "Esposito",
-    "name": "Matteo",
-    "date_birth": "2001-09-30",
-    "email": "matteo.esposito@example.com",
-    "phone_number": "+39 329 654 3210",
-    "notes": "Studente di giurisprudenza."
+    surename: "Trapanese",
+    name: "Marcella",
+    date_birth: "2006-02-28",
+    email: "marcella.trapanese@vodafone.it",
+    phone_number: "+39 8815 20357332",
+    notes: "Aspernatur nobis placeat vitae esse eaque quaerat. Expedita reiciendis praesentium. Tenetur voluptas beatae deleniti."
   },
   {
-    "surname": "Russo",
-    "name": "Francesca",
-    "date_birth": "2002-12-25",
-    "email": "francesca.russo@example.com",
-    "phone_number": "+39 328 567 1234",
-    "notes": "Studentessa di architettura."
+    surename: "Grifeo",
+    name: "Dolores",
+    date_birth: "2005-02-10",
+    email: "dolores.grifeo@fastwebnet.it",
+    phone_number: "+39 59 1711207",
+    notes: "Placeat eveniet possimus necessitatibus doloremque amet modi. Quae minima non maxime. Fuga repudiandae qui."
   },
   {
-    "surname": "Marino",
-    "name": "Andrea",
-    "date_birth": "2003-03-15",
-    "email": "andrea.marino@example.com",
-    "phone_number": "+39 327 234 5678",
-    "notes": "Studente di ingegneria civile."
+    surename: "Ajello",
+    name: "Vincenza",
+    date_birth: "2004-05-02",
+    email: "vincenza.ajello@libero.it",
+    phone_number: "+39 3774 22223280",
+    notes: "Occaecati earum ipsam neque cupiditate totam. Nihil alias eos esse reprehenderit numquam nihil. Molestiae error fugiat aliquid similique."
   },
   {
-    "surname": "Greco",
-    "name": "Chiara",
-    "date_birth": "2002-08-22",
-    "email": "chiara.greco@example.com",
-    "phone_number": "+39 329 345 6789",
-    "notes": "Studentessa di psicologia."
+    surename: "Bevilacqua",
+    name: "Mauro",
+    date_birth: "2005-02-13",
+    email: "mauro.bevilacqua@tiscali.it",
+    phone_number: "+39 6088 72753950",
+    notes: "Sapiente eaque dolores ipsum maiores facere reprehenderit. Eligendi velit repellendus repellendus. Rem iure repellendus quae sint fugit in debitis."
   },
   {
-    "surname": "Conti",
-    "name": "Alessandro",
-    "date_birth": "2001-11-18",
-    "email": "alessandro.conti@example.com",
-    "phone_number": "+39 320 456 7890",
-    "notes": "Studente di biologia."
+    surename: "Meucci",
+    name: "Valentina",
+    date_birth: "2004-09-19",
+    email: "valentina.meucci@tin.it",
+    phone_number: "+39 969 26 96 2713",
+    notes: "Dolores iusto optio quia voluptas asperiores sit. Nemo magnam doloremque explicabo omnis reprehenderit dolor. Repudiandae incidunt expedita vero vitae ab."
   },
   {
-    "surname": "Gallo",
-    "name": "Martina",
-    "date_birth": "2003-02-10",
-    "email": "martina.gallo@example.com",
-    "phone_number": "+39 328 567 8901",
-    "notes": "Studentessa di chimica."
+    surename: "Pometta",
+    name: "Gaspare",
+    date_birth: "2005-03-17",
+    email: "gaspare.pometta@hotmail.it",
+    phone_number: "+39 47 3533641",
+    notes: "Repudiandae cupiditate atque. Quam mollitia quod asperiores error sequi voluptate. Error enim in eaque provident animi. Libero aliquid modi iusto."
   },
   {
-    "surname": "Leone",
-    "name": "Davide",
-    "date_birth": "2002-07-01",
-    "email": "davide.leone@example.com",
-    "phone_number": "+39 327 678 9012",
-    "notes": "Studente di fisica."
+    surename: "Gussoni",
+    name: "Eraldo",
+    date_birth: "2003-12-20",
+    email: "eraldo.gussoni@tele2.it",
+    phone_number: "+39 226 9213314",
+    notes: "Voluptatum exercitationem nisi quibusdam. Suscipit numquam perspiciatis perferendis beatae. Ipsum vero iure minus. Aperiam dicta iste fugit saepe quod inventore."
   },
   {
-    "surname": "Rizzo",
-    "name": "Elisa",
-    "date_birth": "2001-06-20",
-    "email": "elisa.rizzo@example.com",
-    "phone_number": "+39 329 789 0123",
-    "notes": "Studentessa di storia dell'arte."
+    surename: "Caboto",
+    name: "Antonietta",
+    date_birth: "2005-12-24",
+    email: "antonietta.caboto@live.com",
+    phone_number: "+39 099 15489505",
+    notes: "Ut beatae architecto consectetur dolor. Sunt minima ea ratione vel odit nulla. Id quam voluptatem esse delectus."
   },
   {
-    "surname": "Colombo",
-    "name": "Federico",
-    "date_birth": "2002-01-15",
-    "email": "federico.colombo@example.com",
-    "phone_number": "+39 320 890 1234",
-    "notes": "Studente di matematica."
-  },
-  {
-    "surname": "Bruno",
-    "name": "Valentina",
-    "date_birth": "2003-05-10",
-    "email": "valentina.bruno@example.com",
-    "phone_number": "+39 328 901 2345",
-    "notes": "Studentessa di filosofia."
-  },
-  {
-    "surname": "Costa",
-    "name": "Riccardo",
-    "date_birth": "2001-10-30",
-    "email": "riccardo.costa@example.com",
-    "phone_number": "+39 327 012 3456",
-    "notes": "Studente di scienze politiche."
-  },
-  {
-    "surname": "Giordano",
-    "name": "Alice",
-    "date_birth": "2002-04-05",
-    "email": "alice.giordano@example.com",
-    "phone_number": "+39 329 123 4567",
-    "notes": "Studentessa di sociologia."
+    surename: "Valier",
+    name: "Amleto",
+    date_birth: "2005-05-19",
+    email: "amleto.valier@yahoo.com",
+    phone_number: "+39 25 59268152",
+    notes: "Dolore veritatis iure asperiores aut vel. Vel adipisci doloribus quia officiis. Enim ea culpa quas voluptate asperiores hic. Soluta enim impedit corrupti consectetur iste."
   }
 ]

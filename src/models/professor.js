d21f881e2fb6e824cc78063bdde872cd0ace559a
mrskrsm <francescoforgiarini03@gmail.com>
2024-07-15 13:12:42 +0200

    miss pell

--- src/models/professor.js
+++ src/models/professor.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      professor.belongsToMany(models.students,
+      professor.belongsToMany(models.student,
         {
           through: 'request',
           foreignKey: 'professor_id',

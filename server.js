ee95c0f6a3ade32c175f87d5b177b3f34250c928
mrskrsm <francescoforgiarini03@gmail.com>
2024-07-15 10:33:18 +0200

    add: server (fumbled)

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv')
+const express = require('express')
+const PORT = process.env.SERVER_PORT
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('hiiiii')
+})
+
+app.listen(PORT, () => {
+  models.sequelize
+  .authenticate()
+  .then(() => {
+      console.log('Connection successful')
+      console.log(`App listening on http://localhost:${PORT}`)
+  })
+  .catch((error) => {
+      console.log("Error creating connection:", error)
+  })
+})
